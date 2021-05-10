87ca18effb0d5059266d34bffbd3a654cd68a597
neocortex17 <70774115+neocortex17@users.noreply.github.com>
2021-05-10 22:50:51 +0300

    Add files via upload

---
+++ lab6/client/src/main/java/connection/ClientConnectionManager.java
@@ -0,0 +1,11 @@
+package connection;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+
+public interface ClientConnectionManager {
+    DatagramSocket openConnection(String address, int port) throws IOException;
+    SocketAddress getSocketAddress();
+    void closeConnection();
+}
